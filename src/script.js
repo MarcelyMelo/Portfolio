cf920ec8d3323c90db961be94a694c323f9f0730
Marcely Melo <marcelyjfmelo@gmail.com>
2022-07-03 21:21:40 -0300

    fix navbar

--- src/script.js
+++ src/script.js
@@ -1,8 +1,16 @@
 const links = document.querySelector('.nav-links');
 const btn = document.querySelector('.btn-toggle');
 const logo = document.querySelector('.logo')
+const navlink = document.querySelectorAll('.nav-links li a')
 
 btn.addEventListener('click', ()=>{
     links.classList.toggle('show-links')
     logo.classList.toggle('unable')
 })
+
+for(const link of navlink){
+    link.addEventListener('click', ()=>{
+        links.classList.remove('show-links')
+        logo.classList.remove('unable')
+    })
+}
